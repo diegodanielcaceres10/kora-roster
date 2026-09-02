f625a8722e1812a9e9934531d394f45e49990880
Diego Daniel Caceres <diegodanielcaceres10@gmail.com>
2026-09-02 07:42:50 +0200

    translate: Welccome page

--- frontend/src/features/draft/steps/welcome/welcome.tsx
+++ frontend/src/features/draft/steps/welcome/welcome.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { FormattedMessage } from "react-intl";
 import styles from "./welcome.module.scss";
 import koraRosterLogo from "../../../../assets/logo/kora-roster-logo.png";
 import koraBibs from "../../../../assets/illustrations/kora-bibs.png";
@@ -17,35 +18,41 @@ export function StepWelcome({ onStart }: StepWelcomeProps) {
           <img src={koraRosterLogo} alt="" />
         </div>
         <h1 className={styles.welcome__title}>
-          Sorteá los equipos
+          <FormattedMessage id="welcome.title.line1" />
           <br />
-          <span className={styles.welcome__highlight}>en 10 segundos</span>
+          <span className={styles.welcome__highlight}>
+            <FormattedMessage id="welcome.title.highlight" />
+          </span>
         </h1>
 
         <p className={styles.welcome__description}>
-          Juntá a tu gente, armamos los equipos
-          <br />y a jugar. Así de simple.
+          <FormattedMessage id="welcome.description.line1" />
+          <br />
+          <FormattedMessage id="welcome.description.line2" />
         </p>
 
         <div className={styles.welcome__actions}>
           <button type="button" className={styles.welcome__primaryButton} onClick={onStart}>
             <i className="fa-solid fa-user-group"></i>
-            Armar equipos ahora
+            <FormattedMessage id="welcome.actions.primary" />
           </button>
           <button type="button" className={styles.welcome__secondaryButton} onClick={() => navigate("/tutorial")}>
             <i className="fa-solid fa-circle-info"></i>
-            Cómo funciona
+            <FormattedMessage id="welcome.actions.secondary" />
           </button>
         </div>
         <ul className={styles.welcome__stats}>
           <li>
-            <i className="fa-solid fa-gift"></i>100% gratis
+            <i className="fa-solid fa-gift"></i>
+            <FormattedMessage id="welcome.stats.free" />
           </li>
           <li>
-            <i className="fa-solid fa-bolt"></i>Rápido y fácil
+            <i className="fa-solid fa-bolt"></i>
+            <FormattedMessage id="welcome.stats.fast" />
           </li>
           <li>
-            <i className="fa-solid fa-share-nodes"></i>Listo para compartir
+            <i className="fa-solid fa-share-nodes"></i>
+            <FormattedMessage id="welcome.stats.shareable" />
           </li>
         </ul>
       </div>
