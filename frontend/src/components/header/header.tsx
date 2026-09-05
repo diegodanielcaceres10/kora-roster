13aa3cdc63620ee4b7bbaa433db2648b0d90e543
Diego Daniel Caceres <diegodanielcaceres10@gmail.com>
2026-09-05 23:12:07 +0200

    optimize files

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { FormattedMessage, useIntl } from "react-intl";
 import styles from "./header.module.scss";
-import kRosterLogo from "/favicon.svg";
+import kRosterLogo from "/favicon.png";
 import { useAccount } from "../../features/account/AccountContext";
 import { useApiHealth } from "../../features/account/hooks/useApiHealth";
 import { LanguageSwitcher } from "../language-switcher/language-switcher";
